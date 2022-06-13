951d4e80389c3fea3ca651a19326d64757af7822
Berkay Ülger <berkayulger45920@gmail.com>
2022-06-14 02:06:54 +0300

    reg

--- js/script.js
+++ js/script.js
@@ -8,78 +8,86 @@ $(document).ready(function () {
     calculate.text(result)
     
     $('.btn').on('click', function (evt) {
-        var buttonPressed = $(this).html();
-        console.log(buttonPressed);
+        var buttonPress = $(this).html()
+        console.log(buttonPress)
 
-        if (buttonPressed === 'CE') {
+        if (buttonPress === 'CE') {
             result = 0;
             displayEntry = '0';
-        } else if (buttonPressed === '.') {
+        } else if (buttonPress === '.') {
             displayEntry += '.'
-        } else if (buttonPressed === 'del') {
+        } else if (buttonPress === 'del') {
             displayEntry = displayEntry.substring(0, displayEntry.length - 1);
-        } else if (number_true(buttonPressed)) {
-            if (displayEntry === '0') displayEntry = buttonPressed;
-            else if (displayEntry == 'Infinity') displayEntry = buttonPressed;
-            else if (displayEntry == 'NaN') displayEntry = buttonPressed;
-            else if (displayEntry == 'undefined') displayEntry = buttonPressed;
-            else displayEntry = displayEntry + buttonPressed;
-        } else if (number_false(buttonPressed)){
-            if (isOperator(buttonPressed)) {
-                prevEntry = parseFloat(displayEntry);
-                operation = buttonPressed;
-                saveCurrent()
+        } else if (number_true(buttonPress)) {
+            if (displayEntry === '0') displayEntry = buttonPress;
+            else if (displayEntry == 'Infinity') displayEntry = buttonPress;
+            else if (displayEntry == 'NaN') displayEntry = buttonPress;
+            else if (displayEntry == 'undefined') displayEntry = buttonPress;
+            else displayEntry = displayEntry + buttonPress;
+        } else if (number_false(buttonPress)){
+            if (operator_true(buttonPress)) {
+                prevEntry = parseFloat(displayEntry)
+                operation = buttonPress;
+                saveScreen() // Up screen
                 displayEntry = '';
                 
-            } else if (buttonPressed === '%') {
-                displayEntry = displayEntry/100
-                displayEntry = displayEntry + ''
-            } else if (buttonPressed === 'x<sup>2</sup>') {
-                displayEntry *= displayEntry
-                displayEntry = displayEntry + ''
-            } else if (buttonPressed === 'x<sup>3</sup>') {
-                displayEntry = displayEntry ** 3
-                displayEntry = displayEntry + ''
-            } else if (buttonPressed === '10<sup>x</sup>') {
-                displayEntry = 10 ** displayEntry
-                displayEntry = displayEntry + ''
-            } else if (buttonPressed === '<sup>2</sup>√x') {
-                displayEntry = Math.sqrt(displayEntry)
-                displayEntry = displayEntry + ''
-            } else if (buttonPressed === '=') {
-                ans = displayEntry
-                displayEntry = displayEntry 
-                displayEntry = operate(prevEntry, displayEntry, operation);
-                operation = null;
+            } else if (buttonPress === '%') {
+                displayEntry = displayEntry/100;
+                displayEntry = displayEntry + '';
+            } else if (buttonPress === 'x<sup>2</sup>') {
+                displayEntry *= displayEntry;
+                displayEntry = displayEntry + '';
+            } else if (buttonPress === 'x<sup>3</sup>') {
+                displayEntry = displayEntry ** 3;
+                displayEntry = displayEntry + '';
+            } else if (buttonPress === '10<sup>x</sup>') {
+                displayEntry = 10 ** displayEntry;
+                displayEntry = displayEntry + '';
+            } else if (buttonPress === '<sup>2</sup>√x') {
+                displayEntry = Math.sqrt(displayEntry);
+                displayEntry = displayEntry + '';
+
+            } else if (buttonPress === '=') {
+                ans = displayEntry;
+                if (operation != null) {
+                    displayEntry = operate(prevEntry, displayEntry, operation);
+                    operation = null;
+                }
                 console.log('olur');
-                displayEntry = displayEntry + ''
-                ans = displayEntry
+                displayEntry = displayEntry + '';
+                ans = displayEntry;
                 $('.save').html('')
-            } else if (buttonPressed === 'ans') {
-                displayEntry = ans
+            } else if (buttonPress === 'ans') {
+                displayEntry = ans;
             }
 
         }
         updateScreen(displayEntry);
-    });
+    })
+
+    updateScreen = function (displayValue) {
+        var displayValue = displayValue.toString();
+        $('.calculate').html(displayValue.substring(0, 15));
+    }
+
+    saveScreen = function () {
+        var saveEntry = displayEntry.toString();
+        $('.save').html(saveEntry.substring(0, 15) + operation);
+    }
+
 
     number_true = function (value) {
         return !isNaN(value);
     }
+
     number_false = function (value) {
         return isNaN(value);
     }
-    updateScreen = function (displayValue) {
-        var displayValue = displayValue.toString();
-        $('.calculate').html(displayValue.substring(0, 15));
-    };
-    saveCurrent = function(){
-        var saveEntry = displayEntry.toString();
-        $('.save').html(saveEntry.substring(0, 15)+operation);
-    }
-    isOperator = function (value) {
+    
+    operator_true = function (value) {
         return value === '/' || value === 'x' || value === '+' || value === '-';
-    };
+    }
+
     operate = function (a, b, operation) {
         a = parseFloat(a);
         b = parseFloat(b);
@@ -88,9 +96,9 @@ $(document).ready(function () {
         if (operation === '-') return a - b;
         if (operation === 'x') return a * b;
         if (operation === '/') return a / b;
-    }   
+    }
     
-});
+})
 
 
 
